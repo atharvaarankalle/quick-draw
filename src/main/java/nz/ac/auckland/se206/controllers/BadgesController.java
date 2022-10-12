e4414cd83f5a73abe182813675308ac181814b41
atharvaarankalle <atharvanz@gmail.com>
2022-10-12 23:14:45 +1300

    feat: Added routing to badges scene from dashboard

---
+++ src/main/java/nz/ac/auckland/se206/controllers/BadgesController.java
@@ -0,0 +1,23 @@
+package nz.ac.auckland.se206.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+public class BadgesController implements Initializable {
+
+  @FXML private ImageView quickDrawerIcon;
+  @FXML private ImageView speedDemonIcon;
+  @FXML private ImageView speedySketcherIcon;
+  @FXML private ImageView heatingUpIcon;
+  @FXML private ImageView hereComesTheHeatIcon;
+  @FXML private ImageView onFireIcon;
+  @FXML private ImageView aBuddingArtistIcon;
+  @FXML private ImageView seriousSkillsIcon;
+  @FXML private ImageView masterArtistIcon;
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {}
+}
