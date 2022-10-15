511e18e5e0563d8246bb33717c484fdaf9ec3ba6
Yxin-KaiByz <100540976+Yxin-KaiByz@users.noreply.github.com>
2022-10-15 18:17:35 +1300

    Style fixed javadoc and class name for sfx and bgm enums to match convention

--- src/main/java/nz/ac/auckland/se206/controllers/SoundsManager.java
+++ src/main/java/nz/ac/auckland/se206/controllers/SoundsManager.java
@@ -27,27 +27,27 @@ public class SoundsManager {
     private static Media loginSound;
     private static MediaPlayer loginPlayer;
 
-    private static Media mainPanelBGM; // Currently PVZ
+    private static Media mainPanelBackgroundMusic; // Currently PVZ
     private static MediaPlayer mainPanelPlayer;
-    private static Media zenBGM; // Currently PVZ
+    private static Media zenBackgroundMusic; // Currently PVZ
     private static MediaPlayer zenPlayer;
-    private static Media ingameBGM;
+    private static Media ingameBackgroundMusic;
     private static MediaPlayer ingamePlayer;
-    private static Media hiddenBGM;
+    private static Media hiddenBackgroundMusic;
     private static MediaPlayer hiddenPlayer;
 
-    public enum sfx {
+    public enum SoundEffects {
         BUTTON1, BUTTON2, VICTORY, FAIL, BEEP, TAP, PENCIL, ERASER, LOGIN;
     }
 
-    public enum bgm {
+    public enum BackgroundMusic {
         MAINPANEL, INGAME, ZEN, HIDDEN
     }
 
     /**
-     * This method loads all the sfx .mp3 files required for the game
+     * This method loads all the SoundEffects .mp3 files required for the game
      * 
-     * @throws URISyntaxException
+     * @throws URISyntaxException If there is a error in the .mp3 files name
      */
     public static void loadSoundEffects() throws URISyntaxException { 
         //Initilize each Meida field and load the media instance in to its corresponding mediaplayer field
@@ -72,31 +72,31 @@ public class SoundsManager {
     }
 
     /**
-     * This method loads all the bgm .mp3 files required for the game
+     * This method loads all the BackgroundMusic .mp3 files required for the game
      * 
-     * @throws URISyntaxException
+     * @throws URISyntaxException If there is a error in the .mp3 files name
      */
     public static void loadBackgroundMusic() throws URISyntaxException {
-        mainPanelBGM = new Media(App.class.getResource("/sounds/pvzMainMenu.mp3").toURI().toString());
-        mainPanelPlayer = new MediaPlayer(mainPanelBGM);
-        zenBGM = new Media(App.class.getResource("/sounds/zen.mp3").toURI().toString());
-        zenPlayer = new MediaPlayer(zenBGM);
-        ingameBGM = new Media(App.class.getResource("/sounds/ingame.mp3").toURI().toString());
-        ingamePlayer = new MediaPlayer(ingameBGM);
-        hiddenBGM = new Media(App.class.getResource("/sounds/hidden.mp3").toURI().toString());
-        hiddenPlayer = new MediaPlayer(hiddenBGM);
+        mainPanelBackgroundMusic = new Media(App.class.getResource("/sounds/pvzMainMenu.mp3").toURI().toString());
+        mainPanelPlayer = new MediaPlayer(mainPanelBackgroundMusic);
+        zenBackgroundMusic = new Media(App.class.getResource("/sounds/zen.mp3").toURI().toString());
+        zenPlayer = new MediaPlayer(zenBackgroundMusic);
+        ingameBackgroundMusic = new Media(App.class.getResource("/sounds/ingame.mp3").toURI().toString());
+        ingamePlayer = new MediaPlayer(ingameBackgroundMusic);
+        hiddenBackgroundMusic = new Media(App.class.getResource("/sounds/hidden.mp3").toURI().toString());
+        hiddenPlayer = new MediaPlayer(hiddenBackgroundMusic);
     }
 
     /**
-     * Play the specific sfx correspond to the sfx enum input
-     * ERASER and PENCIL sfx will be played in loop
-     * All other sfx will be played once
+     * Play the specific SoundEffects correspond to the SoundEffects enum input
+     * ERASER and PENCIL SoundEffects will be played in loop
+     * All other SoundEffects will be played once
      * 
-     * @param sfxType A sfx enum input, indicating the specific sfx wanted
+     * @param SoundEffectsType A SoundEffects enum input, indicating the specific SoundEffects wanted
      */
-    public static void playSoundEffects(sfx sfxType) {
-        //Play the specific sfx based on the SoundEffects enum input
-        switch (sfxType) {
+    public static void playSoundEffects(SoundEffects SoundEffectsType) { 
+        //Play the specific SoundEffects based on the SoundEffects enum input
+        switch (SoundEffectsType) {
             case BUTTON1:
                 if (buttonPlayer1.getStatus() == MediaPlayer.Status.PLAYING) {
                     buttonPlayer1.stop();
@@ -114,11 +114,11 @@ public class SoundsManager {
                     victoryPlayer.stop();
                 }
                 victoryPlayer.play();
-                //The victory sfx is always supposed to followed up by main panel bgm
+                //The victory SoundEffects is always supposed to followed up by main panel BackgroundMusic
                 victoryPlayer.setOnEndOfMedia(new Runnable() {
                     @Override
                     public void run() {
-                        playBackgroundMusic(bgm.MAINPANEL);
+                        playBackgroundMusic(BackgroundMusic.MAINPANEL);
                     }
                 });
                 break;
@@ -127,11 +127,11 @@ public class SoundsManager {
                     failurePlayer.stop();
                 }
                 failurePlayer.play();
-                //The failure sfx is always supposed to followed up by main panel bgm
+                //The failure SoundEffects is always supposed to followed up by main panel BackgroundMusic
                 failurePlayer.setOnEndOfMedia(new Runnable() {
                     @Override
                     public void run() {
-                        playBackgroundMusic(bgm.MAINPANEL);
+                        playBackgroundMusic(BackgroundMusic.MAINPANEL);
                     }
                 });
                 break;
@@ -149,7 +149,7 @@ public class SoundsManager {
                 break;
             case PENCIL:
                 pencilPlayer.play();
-                //pencil sfx is supposed to be played in endless loop
+                //pencil SoundEffects is supposed to be played in endless loop
                 pencilPlayer.setOnEndOfMedia(new Runnable() {
                     @Override
                     public void run() {
@@ -160,7 +160,7 @@ public class SoundsManager {
                 break;
             case ERASER:
                 eraserPlayer.play();
-                //Eraser sfx is supposed to be played in endless loop
+                //Eraser SoundEffects is supposed to be played in endless loop
                 eraserPlayer.setOnEndOfMedia(new Runnable() {
                     @Override
                     public void run() {
@@ -181,15 +181,15 @@ public class SoundsManager {
     }
 
     /**
-     * Play the specific sfx correspond to the bgm enum input
-     * All bgms will be played in loop
+     * Play the specific SoundEffects correspond to the BackgroundMusic enum input
+     * All BackgroundMusics will be played in loop
      * 
-     * @param bgmType a bgm enum input, indicating the bgm wanted
+     * @param BackgroundMusicType a BackgroundMusic enum input, indicating the BackgroundMusic wanted
      */
-    public static void playBackgroundMusic(bgm bgmType) {
-        //Play the specific bgm corresponding to the BackgroundMusic enum input
-        //All bgms are expected to play in endless loop
-        switch (bgmType) {
+    public static void playBackgroundMusic(BackgroundMusic BackgroundMusicType) {
+        //Play the specific BackgroundMusic corresponding to the BackgroundMusic enum input
+        //All BackgroundMusics are expected to play in endless loop
+        switch (BackgroundMusicType) {
             case MAINPANEL:
                 mainPanelPlayer.play();
                 mainPanelPlayer.setOnEndOfMedia(new Runnable() {
@@ -234,8 +234,8 @@ public class SoundsManager {
     }
 
     /**
-     * This method stops the win/lose sfx, and resume to the looping of main panel
-     * bgm after stopping
+     * This method stops the win/lose SoundEffects, and resume to the looping of main panel
+     * BackgroundMusic after stopping
      */
     public static void stopWinAndLoseSoundEffects() {
         victoryPlayer.stop();
@@ -243,18 +243,18 @@ public class SoundsManager {
     }
 
     /**
-     * This method stops the pencil or eraser sfx from playing.
+     * This method stops the pencil or eraser SoundEffects from playing.
      * Error message will be printed in terminal if invalid input is given
      * 
-     * @param pencilOrEraser a SFX input, indicating weather pencil or eraser is
+     * @param pencilOrEraser a SoundEffects input, indicating weather pencil or eraser is
      *                       required to be stopped
      */
-    public static void stopPencilOrEraserSoundEffects(sfx pencilOrEraser) {
-        if (pencilOrEraser == sfx.PENCIL) {
-            //If passed in SoundEffect enum is PENCIL, stop the pencil sfx
+    public static void stopPencilOrEraserSoundEffects(SoundEffects pencilOrEraser) {
+        if (pencilOrEraser == SoundEffects.PENCIL) {
+            //If passed in SoundEffect enum is PENCIL, stop the pencil SoundEffects
             pencilPlayer.stop();
-        } else if (pencilOrEraser == sfx.ERASER) {
-            //If the passed in SoundEffect enum is ERASER, stop the eraser sfx
+        } else if (pencilOrEraser == SoundEffects.ERASER) {
+            //If the passed in SoundEffect enum is ERASER, stop the eraser SoundEffects
             eraserPlayer.stop();
         } else {
             //If the passed in SoundEffect enum isn't ERASER or PENCIL, print a error message
@@ -263,7 +263,7 @@ public class SoundsManager {
     }
 
     /**
-     * This method stops all curretly playing bgm
+     * This method stops all curretly playing BackgroundMusic
      */
     public static void stopAllBackgroundMusic() {
         zenPlayer.stop();
@@ -273,13 +273,13 @@ public class SoundsManager {
     }
 
     /**
-     * This method stops a specific BGM
+     * This method stops a specific BackgroundMusic
      * 
-     * @param bgmType The specific BGM to be terminated
+     * @param BackgroundMusicType The specific BackgroundMusic to be terminated
      */
-    public static void stopBackgroundMusic(bgm bgmType) {
-        //Stop the specific bgm player corresponding to the BackgroundMusic enum input
-        switch (bgmType) {
+    public static void stopBackgroundMusic(BackgroundMusic BackgroundMusicType) {
+        //Stop the specific BackgroundMusic player corresponding to the BackgroundMusic enum input
+        switch (BackgroundMusicType) {
             case MAINPANEL:
                 mainPanelPlayer.stop();
                 break;
@@ -296,13 +296,14 @@ public class SoundsManager {
     }
 
     /**
-     * This method takes a double as input and change the volume level of all SFX to
+     * This method takes a double as input and change the volume level of all SoundEffects to
      * corresponding value
+     * 
      * 
      * @param volume The disired volume level range from 0 to 100
      */
     public static void changeSoundEffectsVolume(double volume) {
-        //If volume input is valid, apply changes to all sfx players
+        //If volume input is valid, apply changes to all SoundEffects players
         if (volume >= 0 && volume <= 100) {
             buttonPlayer1.setVolume(volume);
             buttonPlayer2.setVolume(volume);
@@ -315,37 +316,37 @@ public class SoundsManager {
             loginPlayer.setVolume(volume);
         } else {
             //Otherwise print error message.
-            System.out.println(volume + " is a invalid sfx volume level");
+            System.out.println(volume + " is a invalid SoundEffects volume level");
         }
     }
 
     /**
-     * This method takes a double as input and change the volume level of all BGM to
+     * This method takes a double as input and change the volume level of all BackgroundMusic to
      * corresponding value
      * 
      * @param volume The disired volume level range from 0 to 100
      */
     public static void changeBackgroundMusicVolume(double volume) {
         if (volume >= 0 && volume <= 100) {
-            //If volume input is valid, apply changes to all bgm players
+            //If volume input is valid, apply changes to all BackgroundMusic players
             mainPanelPlayer.setVolume(volume);
             zenPlayer.setVolume(volume);
             hiddenPlayer.setVolume(volume);
             ingamePlayer.setVolume(volume);
         } else {
             //Otherwise print error message.
-            System.out.println(volume + " is a invalid bgm volume level");
+            System.out.println(volume + " is a invalid BackgroundMusic volume level");
         }
     }
 
     /**
-     * This method changes the mute status of all SFX, according to the boolean
+     * This method changes the mute status of all SoundEffects, according to the boolean
      * input
      * 
      * @param muteStatus boolean input, input false to unmute all
      */
     public static void setMuteAllSoundEffects(Boolean muteStatus) {
-        //Set the mute status of all sfx players
+        //Set the mute status of all SoundEffects players
         buttonPlayer1.setMute(muteStatus);
         buttonPlayer2.setMute(muteStatus);
         victoryPlayer.setMute(muteStatus);
@@ -358,13 +359,13 @@ public class SoundsManager {
     }
 
     /**
-     * This method changes the mute status of all BGM, according to the boolean
+     * This method changes the mute status of all BackgroundMusic, according to the boolean
      * input
      * 
      * @param muteStatus boolean input, input false to unmute all
      */
     public static void setMuteAllBackgroundMusic(Boolean muteStatus) {
-        //Set mute status of all bgm players
+        //Set mute status of all BackgroundMusic players
         mainPanelPlayer.setMute(muteStatus);
         zenPlayer.setMute(muteStatus);
         ingamePlayer.setMute(muteStatus);
@@ -375,7 +376,7 @@ public class SoundsManager {
         return zenPlayer.getStatus() == MediaPlayer.Status.PLAYING;
     }
 
-    public static Boolean ishiddenBGMPlaying() {
+    public static Boolean ishiddenBackgroundMusicPlaying() {
         return hiddenPlayer.getStatus() == MediaPlayer.Status.PLAYING;
     }
 }
