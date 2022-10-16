262cfd66c857fbb7c6392db93bd334e4254b9912
Yxin-KaiByz <100540976+Yxin-KaiByz@users.noreply.github.com>
2022-10-16 16:05:55 +1300

    style added javadoc for the newly added class
    added javadoc to class InGameStatusManager

--- src/main/java/nz/ac/auckland/se206/controllers/InGameStatusManager.java
+++ src/main/java/nz/ac/auckland/se206/controllers/InGameStatusManager.java
@@ -6,11 +6,19 @@ public class InGameStatusManager {
      * when inGameStatus is true, it means the game is currently in running a time line for a game, vice versa
      */
     private static Boolean inGameStatus;
-
+    /**
+     * This method returns the ingame status
+     * 
+     * @return a boolean showing whether a game is running.
+     */
     public static Boolean isInGame() {
         return inGameStatus;
     }
-
+    /**
+     * This method sets the ingame status to desired boolean value
+     * 
+     * @param status the boolean value of the new ingame status
+     */
     public static void setInGameStatus(Boolean status){
         inGameStatus = status;
     }
